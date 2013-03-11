1a63dd32ebaaa0f901f3ca0801be176a87abc989
nosfik <vittalik@i.ua>
2013-03-11 20:56:22 +0200

    eeq

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://realty/admin/');
+define('HTTP_CATALOG', 'http://realty/');
+define('HTTP_IMAGE', 'http://realty/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://realty/admin/');
+define('HTTPS_CATALOG', 'http://realty/');
+define('HTTPS_IMAGE', 'http://realty/image/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:\home\realty\www/admin/');
+define('DIR_SYSTEM', 'Z:\home\realty\www/system/');
+define('DIR_DATABASE', 'Z:\home\realty\www/system/database/');
+define('DIR_LANGUAGE', 'Z:\home\realty\www/admin/language/');
+define('DIR_TEMPLATE', 'Z:\home\realty\www/admin/view/template/');
+define('DIR_CONFIG', 'Z:\home\realty\www/system/config/');
+define('DIR_IMAGE', 'Z:\home\realty\www/image/');
+define('DIR_CACHE', 'Z:\home\realty\www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:\home\realty\www/download/');
+define('DIR_LOGS', 'Z:\home\realty\www/system/logs/');
+define('DIR_CATALOG', 'Z:\home\realty\www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'sa');
+define('DB_DATABASE', 'realty');
+define('DB_PREFIX', '');
+?>
