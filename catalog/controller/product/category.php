4593dbba9a9dc506a9778b98ff0afbb76c8c4c2f
nosfik <vittalik@i.ua>
2013-03-13 21:42:02 +0200

    oio

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -192,15 +192,18 @@ class ControllerProductCategory extends Controller {
 				}
 								
 				$this->data['products'][] = array(
+				
+				
 					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
 					'name'        => $result['name'],
-					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, 100) . '..',
+					//'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, 100) . '..',
 					'price'       => $price,
-					'special'     => $special,
-					'tax'         => $tax,
-					'rating'      => $result['rating'],
-					'reviews'     => sprintf($this->language->get('text_reviews'), (int)$result['reviews']),
+					'city'         	   => $result['city'],
+					'number'           => $result['number'],
+					'area'        	   => $result['area'],
+					'bathroom'         => $result['bathroom'],
+					'badroom'          => $result['badroom'],
 					'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $result['product_id'])
 				);
 			}
